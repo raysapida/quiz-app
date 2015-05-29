9e6fc28fb34617c1d9a7feaabe9209c62af0b58a
Raymond Sapida <ray.sapida@gmail.com>
2015-05-29 09:23:16 -0700

    Add form submit for first question

--- js/app.js
+++ js/app.js
@@ -5,4 +5,15 @@ $(document).ready(function() {
     $("#question-1").css("display", "block");
   });
 
+  $( "form" ).submit(function( event ) {
+    event.preventDefault();
+    var input = $('input[name=q1]:checked', '#question-1').val();
+    $("#question-1").css("display", "none");
+    if (input == 'star') {
+      $("#correct").css("display", "block");
+    } else {
+      $("#wrong").css("display", "block");
+    }
+  });
+
 });
