b38a78cd3fef0aa3512dc4fd5018b6864b5673a3
Raymond Sapida <ray.sapida@gmail.com>
2015-05-29 09:48:00 -0700

    Add question 2 submit function

--- js/app.js
+++ js/app.js
@@ -5,15 +5,45 @@ $(document).ready(function() {
     $("#question-1").css("display", "block");
   });
 
-  $( "form" ).submit(function( event ) {
+  $( " #question-1 form" ).submit(function( event ) {
     event.preventDefault();
     var input = $('input[name=q1]:checked', '#question-1').val();
     $("#question-1").css("display", "none");
     if (input == 'star') {
       $("#correct").css("display", "block");
+      correctNext("#question-2");
     } else {
       $("#wrong").css("display", "block");
+      wrongNext("#question-2");
     }
   });
 
+  $( " #question-2 form" ).submit(function( event ) {
+    event.preventDefault();
+    $("#c-next").unbind("click");
+    $("#w-next").unbind("click");
+    var input = $('input[name=q2]:checked', '#question-2').val();
+    $("#question-2").css("display", "none");
+    if (input == 'grease') {
+      $("#correct").css("display", "block");
+      correctNext("#question-3");
+    } else {
+      $("#wrong").css("display", "block");
+      wrongNext("#question-3");
+    }
+  });
+
+  function correctNext(question) {
+    $("#c-next").click(function() {
+      $("aside").css("display", "none");
+      $(question).css("display", "block");
+    });
+  }
+
+  function wrongNext(question) {
+    $("#w-next").click(function() {
+      $("aside").css("display", "none");
+      $(question).css("display", "block");
+    });
+  }
 });
